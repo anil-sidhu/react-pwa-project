2418efbcd9bcfaf1726f79377e266233999e79c4
anil <webanilsidhu@gmail.com>
2020-08-11 00:31:45 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,42 @@
+let cacheData = "appV1";
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                '/static/js/main.chunk.js',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js',
+                '/static/css/main.chunk.css',
+                '/bootstrap.min.css',
+                '/index.html',
+                '/',
+                "/users"
+            ])
+        })
+    )
+})
+this.addEventListener("fetch", (event) => {
+
+
+    // console.warn("url",event.request.url)
+
+
+    if (!navigator.onLine) {
+        if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
+            event.waitUntil(
+                this.registration.showNotification("Internet", {
+                    body: "internet not working",
+                })
+            )
+        }
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if (resp) {
+                    return resp
+                }
+                let requestUrl = event.request.clone();
+                fetch(requestUrl)
+            })
+        )
+    }
+}) 
