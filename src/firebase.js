681339ff0831f2076d63c475e4607605a18f8d4d
anil <webanilsidhu@gmail.com>
2020-08-11 00:23:23 +0530

    firebase done

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ var firebaseConfig = {
     databaseURL: "https://pwa-app-ccb69.firebaseio.com",
     projectId: "pwa-app-ccb69",
     storageBucket: "pwa-app-ccb69.appspot.com",
-    messagingSenderId: "",
+    messagingSenderId: "87598063745",
     appId: "1:875918063745:web:366bb079c60e9d08cdb580"
   };
   // Initialize Firebase
